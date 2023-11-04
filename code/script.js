cecd4f5b2424bc06b081dcfdb5df5dfabc59f5de
prwt888 <priyanshrawat888@gmail.com>
2023-11-04 14:50:57 +0530

    new commits

--- code/script.js
+++ code/script.js
@@ -25,3 +25,32 @@ arr.forEach(button => {
         
     })
 })
+document.addEventListener('keydown', function(event) {
+    if (event.key >= 0 && event.key <= 9) { // Numbers
+        string += event.key;
+    } else if (event.key === 'Backspace' && !event.shiftKey) {
+        // Only trigger delete when Shift is not pressed
+        string = string.substring(0, string.length - 1);
+    } else if (event.key === 'Enter') {
+        // Try to evaluate the expression and catch any errors
+        try {
+            string = eval(string).toString();
+        } catch (e) {
+            console.error(e);
+            string = ""; // Optionally reset the string on error
+        }
+    } else if (['/', '*', '-', '+', '%'].includes(event.key)) {
+        string += event.key;
+    } else if (event.key === 'Backspace' && event.shiftKey) {
+        // Clear everything if Shift + Backspace is pressed
+        string = "";
+    }
+    input.value = string; // Update the display after every key press
+});
+
+input.addEventListener('keyup', function(event) {
+    // This prevents any default action associated with the Enter key
+    if (event.key === 'Enter') {
+        event.preventDefault();
+    }
+});
